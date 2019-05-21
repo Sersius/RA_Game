e83c78eaa762ae57ca7bc1783691a87820653c2c
Acaree <acampamar@gmail.com>
2019-05-21 04:03:52 +0200

    Player control done!

---
+++ Ra_project/Assets/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float bullet_speed = 10.0f;
+    public GameObject bullet_dir;
+    private Vector3 direction;
+    public float bullet_lifetime = 7.0f;
+    private float bullet_time = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = bullet_dir.transform.position - gameObject.transform.position;
+        direction.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(direction * bullet_speed * Time.deltaTime, Space.World);
+        bullet_time += Time.deltaTime;
+        if(bullet_time > bullet_lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Ra_project/Assets/Player_Shooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Shooter : MonoBehaviour
+{
+
+    public GameObject bullet_prefab;
+    public GameObject bullet_position;
+    public float time_between_shoots = 1.0f;
+    private float shoot_timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot_timer += Time.deltaTime;
+        if(shoot_timer > time_between_shoots)
+        {
+            GameObject new_bullet = Instantiate(bullet_prefab);
+            new_bullet.transform.position = gameObject.transform.position;
+            new_bullet.transform.rotation = gameObject.transform.rotation;
+            new_bullet.SetActive(true);
+            shoot_timer = 0.0f;
+        }
+    }
+}
