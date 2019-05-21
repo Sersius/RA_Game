f5f9981fe303541d2c42ae53c4db14d44c931b2e
Sersius <segoro97@msn.com>
2019-05-22 00:46:18 +0200

    Explosions and some more things that i dont remember

--- Ra_project/Assets/Bullet.cs
+++ Ra_project/Assets/Bullet.cs
@@ -9,6 +9,8 @@ public class Bullet : MonoBehaviour
     public float bullet_speed = 10.0f;
     public GameObject bullet_dir;
     public GameObject canvas;
+    public GameObject explosion_o;
+    public GameObject explosion_b;
     private Vector3 direction;
     public float bullet_lifetime = 2.5f;
     private float bullet_time = 0.0f;
@@ -34,19 +36,23 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.layer == 10)
+        if (other.gameObject.layer == 10)
         {
             canvas.transform.GetComponent<points>().points_b += 1;
             canvas.transform.GetChild(0).GetComponent<Text>().text = canvas.transform.GetComponent<points>().points_b.ToString();
+            GameObject particles = Instantiate(explosion_o, other.gameObject.transform.GetChild(0).transform);
+            particles.transform.GetComponent<ParticleSystem>().Play();
         }
 
-        else if (other.gameObject.layer == 11)
+        if (other.gameObject.layer == 11)
         {
             canvas.transform.GetComponent<points>().points_a += 1;
             canvas.transform.GetChild(1).GetComponent<Text>().text = canvas.transform.GetComponent<points>().points_a.ToString();
+            GameObject particles = Instantiate(explosion_b, other.gameObject.transform.GetChild(0).transform);
+            particles.transform.GetComponent<ParticleSystem>().Play();
         }
 
-        else if (other.gameObject.layer == 9)
+        if (other.gameObject.layer == 9)
         {
             direction = -direction;
         }
