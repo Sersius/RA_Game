a6b24358e500a601a547a3c35f04fa829275068c
Sersius <segoro97@msn.com>
2019-05-21 09:58:55 +0200

    Added second player, and shoots good

--- Ra_project/Assets/Bullet.cs
+++ Ra_project/Assets/Bullet.cs
@@ -8,7 +8,7 @@ public class Bullet : MonoBehaviour
     public float bullet_speed = 10.0f;
     public GameObject bullet_dir;
     private Vector3 direction;
-    public float bullet_lifetime = 7.0f;
+    public float bullet_lifetime = 4.0f;
     private float bullet_time = 0.0f;
 
     // Start is called before the first frame update
