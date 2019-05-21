22b2e4908c542fac709da86e89a77a6174017fda
Acaree <acampamar@gmail.com>
2019-05-21 05:56:52 +0200

    Enemies behaviour part 1

---
+++ Ra_project/Assets/freeze_y.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class freeze_y : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform Initial_rot;
+
+    void Start()
+    {
+        Initial_rot = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(gameObject.transform.position.x, Initial_rot.position.y, gameObject.transform.position.z, Space.World);
+    }
+}
